bd23d2175bf9d214c1146d6a9100a3a16d7ea401
disasteroftheuniverse <patrick@patrickpineda.net>
2025-12-09 15:01:44 -0700

    updated to module type

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 import localtunnel from 'localtunnel';
 import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
+
+/**
+ * Generate a timestamp in miliseconds
+ * @returns {String} timestamp in miliseconds
+ */
+function GetTimestamp() {
+    return new Date().getTime();
+}
 
 /**
  * Creates a VPN tunnel exposed to the outside network; LocalTunnel is prone to crashes, this program helps keep it online
@@ -9,25 +18,56 @@ import dotenv from 'dotenv';
  * @param {Number} delay miliseconds to delay beteween restarting
  * @returns {tunnel}
  */
-async function StartTunnel(port=4455, subdomain, autorestart, delay) {
-    return new Promise((resolve,reject)=>{
+async function StartTunnel(port = 4455, subdomain, autorestart, delay) {
+    //I wish js had type casting
+    port = Number(port);
+    subdomain = String(subdomain);
+    autorestart = Boolean(autorestart);
+    delay = Number(delay);
+    return new Promise((resolve, reject) => {
         localtunnel({ port, subdomain })
-        .then(tunnel=>{
-            console.log(`${Date.now}: Local Tunnel started at ${tunnel.url}:${port}`);
+            .then(tunnel => {
+                //log restarts
+                console.log(`${GetTimestamp()}: Local Tunnel started at ${tunnel.url}:${port}`);
 
-            tunnel.on('request',(info)=>{
-                var msg = JSON.stringify(info);
-                console.log(`${Date.now}: ${msg}`);
-            })
+                //log requests, makes the program feel like its doing something
+                tunnel.on('request', (info) => {
+                    console.log(GetTimestamp(), info);
+                })
 
-            tunnel.on('error',(err)=>{
-                return reject(err);
+                // localtunnel is fond of crashing for no obvious reason, 
+                // this will happen often, end the promise,
+                // catch the error and move on to error handling
+                tunnel.on('error', (err) => {
+                    return reject(err);
+                })
             })
-        })
-        .catch(err=>{
-            var msg = JSON.stringify(err);
-            console.log(`${Date.now}: ${msg}`);
-            return resolve(StartTunnel(port, subdomain))
-        })
+            .catch(err => {
+                //log errors
+                console.log(GetTimestamp(), err)
+
+                //this should be considered the default
+                if (autorestart) {
+                    if (delay) {
+                        setTimeout(() => {
+                            return resolve(StartTunnel(port, subdomain, autorestart, delay));
+                        }, delay);
+                    }
+                    else {
+                        return resolve(StartTunnel(port, subdomain, autorestart, delay));
+                    }
+                } else {
+                    return resolve(process.exit(1));
+                }
+            })
     })
 }
+
+
+function main() {
+    let {PORT , AUTORESTART, SUBDOMAIN, DELAY } = process.env;
+    StartTunnel( PORT, SUBDOMAIN, AUTORESTART, DELAY );
+}
+
+
+main();
